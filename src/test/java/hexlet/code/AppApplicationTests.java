fe81580e00c7db56c87446ddd2bd048114d14331
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-07-10 19:04:54 +0300

    add fixes

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -3,11 +3,14 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class AppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testInit() {
+		assertThat(true).isTrue();
 	}
 
 }
