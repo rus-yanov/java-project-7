5496cfdcd9d742a4ad851b5f349fbfa6bff1c9eb
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-07-17 10:47:29 +0300

    add rollbar

---
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -0,0 +1,37 @@
+package hexlet.code.config.rollbar;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"hexlet.code"})
+public class RollbarConfig {
+
+    // Добавляем токен через переменные окружения
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    @Bean
+    public Rollbar rollbar() {
+
+        return new Rollbar(getRollbarConfigs(rollbarToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("development")
+                .enabled(activeProfile == "prod")
+                .build();
+    }
+}
